d5bd015f3b8e009996c56fd3e3d083ea2a09d8ec
edelsid <eradonna@yandex.ru>
2025-01-21 01:56:39 +0300

    card opening animation

--- src/components/works/WorkList.jsx
+++ src/components/works/WorkList.jsx
@@ -1,16 +1,22 @@
 export default function WorkList() {
   const expand = (e) => {
+    const data = e.target.getBoundingClientRect();
+    document.body.style.overflow = "hidden";
+    e.target.style.transform = `translateX(-${data.x}px)
+      translateY(-${data.y + 10}px)`;
+    e.target.classList.remove("card_closed");
+    e.target.classList.add("card_open");
   }
 
   return (
     <ul className="work__list">
-      <li className="work__item" onClick={expand}>
+      <li className="card card_closed" onClick={expand}>
         <h3 className="title title_work">Работа 1</h3>
       </li>
-      <li className="work__item" onClick={expand}>
+      <li className="card card_closed" onClick={expand}>
         <h3 className="title title_work">Работа 2</h3>
       </li>
-      <li className="work__item" onClick={expand}>
+      <li className="card card_closed" onClick={expand}>
         <h3 className="title title_work">Работа 3</h3>
       </li>
     </ul>
